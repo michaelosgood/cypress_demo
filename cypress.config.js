012cb2ec14c931d168ac30e94262549a0b69edaf
Michael Osgood <michael.f.osgood@gmail.com>
2024-05-05 20:20:57 -0700

    updated viewport height/width on config

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,9 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  viewportWidth: 1080,
+  viewportHeight: 720,
+  pageLoadTimeout: 200000,
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
