1741797a9d7be427d11d724424cab613194edd05
Michael Osgood <michael.f.osgood@gmail.com>
2024-05-07 16:29:12 -0700

    added developer portfoio tests

--- cypress.config.js
+++ cypress.config.js
@@ -5,6 +5,7 @@ module.exports = defineConfig({
   viewportHeight: 720,
   pageLoadTimeout: 200000,
   e2e: {
+    excludeSpecPattern: ['**/1-getting-started', '**/2-advanced-examples'],
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
---
+++ cypress/e2e/portfolio_tests/firstTest.cy.js
@@ -0,0 +1,27 @@
+/// <reference types="Cypress" />
+
+
+describe('Developer Portfolio Test Suite', () => {
+    context('(Portfolio Tests)', () => { 
+       
+        beforeEach(() => {
+            cy.openHomePage()
+        })
+
+        it('Verify profile image is visible', () => {
+            cy.contains('Home').click()
+            cy.get('#profile-image').should('be.visible')
+        })
+
+        it('Verify projects section', () => {
+            cy.contains('Projects').click()
+            cy.get('#project-link').should('contain', 'Projects')
+        })
+
+        it('Verify contact section', () => {
+            cy.contains('Contact').click()
+            cy.get('.contact-me').should('contain', 'Contact Me')
+        })
+
+    })
+})
